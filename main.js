49a96bf0dea79891472665006f6a36ca33cae32b
Barathsaraan <saraan.barath@gmail.com>
2023-07-12 12:29:39 +0530

    add

---
+++ main.js
@@ -0,0 +1,11 @@
+// elif
+let num=prompt("Enter your number");
+if(num>0){
+	document.write("Positive");
+}
+else if(num==0){
+	document.write("Number is 0");
+}
+else{
+	document.write("Negative");
+}
